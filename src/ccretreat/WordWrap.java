7d94ab3f2cd2b08d13410c61ff9d321f0891cf6c
albeja <albeja@WZDE8272.D0ZDE.HORNBACH.INT>
2017-05-17 09:41:30 +0200

    initial umbrechen and wörterUmbrechen implementations

---
+++ src/ccretreat/WordWrap.java
@@ -0,0 +1,18 @@
+package ccretreat;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WordWrap {
+
+	public static String umbrechen(String text, int maxZeilenlänge){
+		return text;
+	}
+
+	static String[] wörterUmbrechen(String text) {
+		return Stream.of(text.split(" "))
+				.filter(s -> !s.isEmpty())
+				.collect(Collectors.toList())
+				.toArray(new String[]{});
+	}
+}
---
+++ src/ccretreat/WordWrapTest.java
@@ -0,0 +1,36 @@
+package ccretreat;
+
+import static org.hamcrest.CoreMatchers.is;
+import static ccretreat.WordWrap.umbrechen;
+import static ccretreat.WordWrap.wörterUmbrechen;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WordWrapTest {
+	
+	@Test
+	public void textWithSingleLetterLinesWrapsAtLengthOne() {
+		assertThat(umbrechen("a\nb\nc", 1), is("a\nb\nc"));
+	}
+	
+	@Test
+	public void wordsAreSeperatedByOneSpace(){
+		assertThat(wörterUmbrechen("a b c"), is(new String[]{"a", "b", "c"}));
+	}
+	
+	@Test
+	public void surroundingSpacesAreIgnored() {
+		assertThat(wörterUmbrechen(" a b c "), is(new String[]{"a", "b", "c"}));
+	}
+	
+	@Test
+	public void multipleSpacesAreIgnored() throws Exception {
+		assertThat(wörterUmbrechen("a  b c  "), is(new String[]{"a", "b", "c"}));
+	}
+	
+	@Test
+	public void commasStickToWord() throws Exception {
+		assertThat(wörterUmbrechen("a ,b c,"), is(new String[]{"a", ",b", "c,"}));
+	}
+}
