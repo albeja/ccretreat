9ecf2d2c41fbbd8eef989229984d6df667ec2e40
albeja <albeja@WZDE8272.D0ZDE.HORNBACH.INT>
2017-05-17 11:29:43 +0200

    Remove obsolete import

--- src/ccretreat/WordWrapTest.java
+++ src/ccretreat/WordWrapTest.java
@@ -2,7 +2,6 @@ package ccretreat;
 
 import static org.hamcrest.CoreMatchers.is;
 import static ccretreat.WordWrap.umbrechen;
-import static ccretreat.WordWrap.wörterUmbrechen;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -13,7 +12,6 @@ public class WordWrapTest {
 	public void textWithSingleLetterLinesWrapsAtLengthOne() {
 		assertThat(umbrechen("a\nb\nc", 1), is("a\nb\nc"));
 	}
-	
 	
 	@Test
 	public void wordsAreYankedWithNewline() {
